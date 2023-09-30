1779cef0e3deb379995e722b07a8c356f1544816
ccireedui <ireedui355@gmail.com>
2023-10-01 04:26:15 +0800

    added getters for brc20s and ordinals

--- src/scripts/balance_getter.js
+++ src/scripts/balance_getter.js
@@ -1,46 +1,141 @@
-const mempoolJS = require('@mempool/mempool.js')
-const axios = require('axios')
-require('dotenv').config()
+const mempoolJS = require("@mempool/mempool.js");
+const axios = require("axios");
+const he = require("he");
+require("dotenv").config();
 
 async function main() {
   const address =
-    'tb1pk4dzxehzkcmqk3c685gukuhjamvcs690tdlemrcrvttjy273gqmsrh2us5'
-  const balance = await getBalance(address)
-  console.log(balance)
+    "tb1pk4dzxehzkcmqk3c685gukuhjamvcs690tdlemrcrvttjy273gqmsrh2us5";
+  const balance = await getOrdinals(address);
+  console.log(balance);
 }
 
-async function getBalance(address) {
+async function getInscriptions(address) {
   try {
-    let inscriptions = []
+    let inscriptions = [];
     const {
       bitcoin: { addresses },
     } = mempoolJS({
-      hostname: 'mempool.space',
-      network: 'testnet',
-    })
+      hostname: "mempool.space",
+      network: "testnet",
+    });
 
-    const transactions = await addresses.getAddressTxsUtxo({ address })
+    const transactions = await addresses.getAddressTxsUtxo({ address });
     for (let i = 0; i < transactions.length; i++) {
       try {
         const response = await axios.get(
-          `https://testnet.ordinals.com/preview/${transactions[i].txid}i0`
-        )
+          `https://testnet.ordinals.com/preview/${transactions[i].txid}i0`,
+        );
         if (response.status === 200) {
-          inscriptions.push(transactions[i].txid)
+          inscriptions.push(transactions[i].txid);
         }
       } catch (error) {
-        // Handle specific error for 404 (Not Found) status code
         if (error.response && error.response.status === 404) {
         } else {
           console.error(
             `Error processing transaction ${transactions[i].txid}:`,
-            error
-          )
+            error,
+          );
         }
       }
     }
-    return inscriptions
+    return inscriptions;
   } catch (error) {}
 }
 
-main().catch(console.error)
+async function getOrdinals(address) {
+  try {
+    let ordinals = [];
+    const {
+      bitcoin: { addresses },
+    } = mempoolJS({
+      hostname: "mempool.space",
+      network: "testnet",
+    });
+
+    const transactions = await addresses.getAddressTxsUtxo({ address });
+    for (let i = 0; i < transactions.length; i++) {
+      try {
+        const response = await axios.get(
+          `https://testnet.ordinals.com/preview/${transactions[i].txid}i0`,
+        );
+        if (response.status === 200) {
+          const htmlResponse = response.data;
+          const inscriptionIdStartIndex =
+            htmlResponse.lastIndexOf("<img src=/content/");
+          const inscriptionIdEndIndex = htmlResponse.lastIndexOf("></img>\n");
+          const inscriptionId = htmlResponse.substring(
+            inscriptionIdStartIndex,
+            inscriptionIdEndIndex + 1,
+          );
+
+          if (inscriptionId) {
+            ordinals.push(transactions[i].txid);
+          }
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+        } else {
+          console.error(
+            `Error processing transaction ${transactions[i].txid}:`,
+            error,
+          );
+        }
+      }
+    }
+    return ordinals;
+  } catch (error) {}
+}
+
+async function getBRC20s(address) {
+  try {
+    let brc20s = [];
+    const {
+      bitcoin: { addresses },
+    } = mempoolJS({
+      hostname: "mempool.space",
+      network: "testnet",
+    });
+
+    const transactions = await addresses.getAddressTxsUtxo({ address });
+    for (let i = 0; i < transactions.length; i++) {
+      try {
+        const response = await axios.get(
+          `https://testnet.ordinals.com/preview/${transactions[i].txid}i0`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+          },
+        );
+        if (response.status === 200) {
+          const htmlResponse = response.data;
+          const jsonStartIndex = htmlResponse.lastIndexOf("{");
+          const jsonEndIndex = htmlResponse.lastIndexOf("}");
+          const jsonString = htmlResponse.substring(
+            jsonStartIndex,
+            jsonEndIndex + 1,
+          );
+          const decodedJsonString = he.decode(jsonString);
+          const jsonData = JSON.parse(decodedJsonString);
+
+          if (jsonData.p && jsonData.op && jsonData.tick && jsonData.amt) {
+            brc20s.push(transactions[i].txid);
+          }
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+        } else {
+          console.error(
+            `Error processing transaction ${transactions[i].txid}:`,
+            error,
+          );
+        }
+      }
+    }
+    return brc20s;
+  } catch (error) {}
+}
+
+main().catch(console.error);
