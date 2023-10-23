3bb8f99bab1c2d1e1719ddbad92625ceddc7f169
ccireedui <ireedui355@gmail.com>
2023-10-24 00:43:12 +0800

    added testnet brc20 support

---
+++ src/scripts/balance_getter_unisat_testnet.js
@@ -0,0 +1,161 @@
+const mempoolJS = require("@mempool/mempool.js");
+const axios = require("axios");
+const he = require("he");
+require("dotenv").config();
+
+const apiKey = process.env.UNISAT_API_KEY;
+const apiUrl = "https://open-api-testnet.unisat.io";
+
+async function main() {
+  const address = "tb1qe8h5ensyqt04m64a5wwf60xy3dp0fta5dguwzy";
+  const balance = await getBRC20s(address);
+  console.log(balance);
+}
+
+async function getBRC20s(address) {
+  const transactions = await getBRC20transactions(address);
+  const finalBalance = transactions.reduce((acc, cur) => {
+    acc[cur.ticker] = acc[cur.ticker] || { balance: 0, transferable: 0 };
+    if (cur.operation === "mint") {
+      acc[cur.ticker].balance += parseFloat(cur.amount);
+    } else if (cur.operation === "transfer") {
+      if (cur.sequence === 0 && cur.moved === true) {
+        acc[cur.ticker].balance -= parseFloat(cur.amount);
+      } else if (cur.sequence === 0 && cur.moved === false) {
+        acc[cur.ticker].transferable += parseFloat(cur.amount);
+      } else if (cur.sequence === 1) {
+        acc[cur.ticker].balance += parseFloat(cur.amount);
+      }
+    }
+    return acc;
+  }, {});
+  return finalBalance;
+}
+
+async function getBRC20transactions(address) {
+  let inscriptions = [];
+  const utxoLength = await getUTXOLength(address);
+  try {
+    const response = await axios.get(
+      `${apiUrl}/v1/indexer/address/${address}/inscription-utxo-data/?size=${utxoLength}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      },
+    );
+    const utxos = response.data.data.utxo;
+    inscriptions = await processUtxos(utxos);
+  } catch (error) {
+    console.log(error);
+  }
+  return inscriptions;
+}
+
+const processUtxos = async (utxos) => {
+  const inscriptions = await Promise.all(
+    utxos.flatMap(async (utxo) => {
+      if (
+        utxo.inscriptions &&
+        utxo.inscriptions[0] !== undefined &&
+        utxo.inscriptions[0] !== null &&
+        utxo.inscriptions[0].isBRC20
+      ) {
+        try {
+          const brc20Info = await processBRC20(
+            utxo.inscriptions[0].inscriptionId,
+          );
+          const mergedInscriptionInfo = {
+            ...utxo.inscriptions[0],
+            ...brc20Info,
+          };
+          return mergedInscriptionInfo;
+        } catch (error) {
+          // Handle errors from processBRC20 function
+          console.error("Error processing BRC20:", error);
+        }
+      }
+      return null; // Return null for invalid or unprocessable inscriptions
+    }),
+  );
+
+  // Filter out null values (failed processing) and return the resulting array
+  return inscriptions.filter((inscription) => inscription !== null);
+};
+
+async function processBRC20(inscriptionId) {
+  try {
+    const response = await axios.get(
+      `https://testnet.ordinals.com/preview/${inscriptionId}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      },
+    );
+
+    if (response.status === 200) {
+      const htmlResponse = response.data;
+      const jsonStartIndex = htmlResponse.lastIndexOf("{");
+      const jsonEndIndex = htmlResponse.lastIndexOf("}");
+      const jsonString = htmlResponse.substring(
+        jsonStartIndex,
+        jsonEndIndex + 1,
+      );
+      const decodedJsonString = he.decode(jsonString);
+      const jsonData = JSON.parse(decodedJsonString);
+
+      if (jsonData.p && jsonData.op && jsonData.tick && jsonData.amt) {
+        const brc20Info = {
+          amount: jsonData.amt,
+          ticker: jsonData.tick,
+          operation: jsonData.op,
+          type: jsonData.p,
+        };
+        return brc20Info;
+      } else if (
+        jsonData.p &&
+        jsonData.op &&
+        jsonData.tick &&
+        jsonData.max &&
+        jsonData.lim
+      ) {
+        const brc20Info = {
+          max: jsonData.max,
+          limit: jsonData.lim,
+          ticker: jsonData.tick,
+          operation: jsonData.op,
+          type: jsonData.p,
+        };
+        return brc20Info;
+      }
+    }
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      // Handle 404 error if needed
+    } else {
+      console.error(`Error processing inscription ${inscriptionId}:`, error);
+    }
+  }
+}
+
+async function getUTXOLength(address) {
+  let length = 0;
+  try {
+    const response = await axios.get(
+      `${apiUrl}/v1/indexer/address/${address}/inscription-utxo-data`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      },
+    );
+    length = response.data.data.totalConfirmed;
+  } catch (error) {
+    console.log(error);
+  }
+  return length;
+}
+
+main().catch(console.error);
